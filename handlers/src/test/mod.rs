a3929999dd44bdfc4e75ddb54813ed9df51d1371
caiuschou <caiuschou@gmail.com>
2026-01-31 14:41:00 +0900

    chore: remove orphaned handlers directory

--- handlers/src/test/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Unit test module
-//!
-//! Handler unit tests live here, separate from source files.
-//! Tests interact with handlers via public and pub(crate) APIs.
-
-mod logging_auth_handler_test;
-mod persistence_handler_test;
